1c34a635088a3a1db3de48e6d2b55048bfbcf170
Jeremiah <jeremiah.fallin@gmail.com>
2020-12-29 11:58:47 -0800

    way better. still some functionality improvements left.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import Playlists from "./Playlists";
 // import Songs from "./Songs";
 import PlayerBar from "./PlayerBar";
